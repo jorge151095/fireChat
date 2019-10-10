94f0ebd1f5b097cf2ef05a6987e85118387918b6
Jorge Antonio HG <jorgeantoniohernandezguzman@gmail.com>
2019-10-10 12:32:15 -0500

    init ptoject

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAKM9LUMkx9Lp129a_0w3KKyCfza8A5hzA",
+    authDomain: "firechat-85d06.firebaseapp.com",
+    databaseURL: "https://firechat-85d06.firebaseio.com",
+    projectId: "firechat-85d06",
+    storageBucket: "firechat-85d06.appspot.com",
+    messagingSenderId: "818545301203",
+    appId: "1:818545301203:web:bcfdb60eb875acf21b6626",
+    measurementId: "G-3JCYD9XX7W"
+  }
 };
 
 /*
